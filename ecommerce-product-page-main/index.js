0043662514605f896795fbcdba26fbe50dc5bf76
feranmygit <feranmygit@gmail.com>
2024-02-26 14:27:16 -0800

    i_modified_new_code_in_HTML_CSS_and_JavaScript

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -1,5 +1,18 @@
 
 function changeBackground(Big) {
-    var backgroundImage = window.getComputedStyle(Big).backgroundImage;
+    const backgroundImage = window.getComputedStyle(Big).backgroundImage;
     element.style.backgroundImage = backgroundImage;
+    
+}
+var currentIndex = 0;
+var picture = document.querySelectorAll('.picture');
+var element = document.getElementById('element');
+  
+function nextBackground() {
+    currentIndex = (currentIndex + 1) % picture.length;
+    changeBackground(picture[currentIndex]);
+}
+function prevBackground() {
+    currentIndex = (currentIndex - 1 + picture.length) % picture.length;
+    changeBackground(picture[currentIndex]);
 }
