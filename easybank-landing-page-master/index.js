7107115557655754812737f2ef0bf14b6107998b
feranmygit <feranmygit@gmail.com>
2024-03-09 07:50:52 -0800

    i make some edition to my styling

---
+++ easybank-landing-page-master/index.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const hambugger = document.querySelector('.hambugger');
+    const removeContainer = document.querySelector('#removeContainer');
+    
+    if (hambugger.style.display === 'none') {
+        hambugger.style.display = 'block';
+        removeContainer.style.display = 'none';
+    }
+    else {
+        hambugger.style.display = 'none';
+        removeContainer.style.display = 'block';
+    }
+
+}
