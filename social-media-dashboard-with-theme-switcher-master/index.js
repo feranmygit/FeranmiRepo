ef4f2376586da59608cbc9c40dab1fdd03cb25c5
feranmygit <feranmygit@gmail.com>
2024-03-17 12:54:36 +0100

    i change the mobile view screen size

--- social-media-dashboard-with-theme-switcher-master/index.js
+++ social-media-dashboard-with-theme-switcher-master/index.js
@@ -29,3 +29,29 @@ function updateContainers(){
 document.addEventListener('DOMContentLoaded', function() {
     updateContainers();
 })
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    const hiddenContainer = document.querySelectorAll('.hiddenContainer');
+    hiddenContainer.forEach(hiddenContainer => {
+        if (isInViewport(hiddenContainer)) {
+            hiddenContainer.classList.add('visible');
+            hiddenContainer.classList.remove('hidden');
+        } else {
+            hiddenContainer.classList.remove('visible');
+            hiddenContainer.classList.add('hidden');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
