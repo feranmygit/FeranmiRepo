8b96b6d51c509b6bd41f0af80bd06c61bff50c6f
feranmygit <feranmygit@gmail.com>
2024-03-17 06:47:44 +0100

    i completed the theme switcher dark mode

--- try.js
+++ try.js
@@ -1,4 +1,3 @@
-
 function toggleTheme() {
     document.body.classList.toggle('light-mode');
     document.body.classList.toggle('dark-mode');
@@ -15,11 +14,11 @@ function updateContainers(){
         const containerSelector = `#${containerId}`;
 
         if (mode === 'light-mode') {
-            container.classList.remove('light-mode');
+            container.classList.remove('dark-mode');
             container.classList.remove(`${containerId}-dark-mode`);
             container.classList.add(`${containerId}-light-mode`);
         } else {
-            container.classList.remove('dark-mode');
+            container.classList.remove('light-mode');
             container.classList.remove(`${containerId}-light-mode`);
             container.classList.add(`${containerId}-dark-mode`);
         }
@@ -28,4 +27,19 @@ function updateContainers(){
 
 document.addEventListener('DOMContentLoaded', function() {
     updateContainers();
+})
+
+
+
+const toggleTheme = document.getElementById('toggleTheme');
+const container = document.querySelectorAll('.container');
+const body = document.body;
+
+toggleTheme.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+    container.forEach(container => {
+        container.classList.toggle('light-mode');
+        container.classList.toggle('dark-mode');
+    })
 })
