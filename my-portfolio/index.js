d79598764558a0011a226e12b5f75f520ef013d5
feranmygit <feranmygit@gmail.com>
2024-03-28 23:38:42 -0700

    I added the styling to it

---
+++ my-portfolio/index.js
@@ -0,0 +1,9 @@
+const text = document.getElementById('text');
+
+function typeText() {
+    let index= 0;
+    const typingInterval = setInterval(() => {
+        tyingText.style.display = 'inline';
+        tyingText.textCo
+    })
+}
