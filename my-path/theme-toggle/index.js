e0230ca90fce61ac8ba4ae637455e91dcba8754f
feranmi <feranmygit@gmail.com>
2024-04-11 16:33:28 -0700

    made some changes to my stylings

---
+++ my-path/theme-toggle/index.js
@@ -0,0 +1,27 @@
+function activateTheme(themeId) {
+    // Disable all CSS files
+    var cssFiles = document.querySelectorAll('link[rel="stylesheet"]');
+    cssFiles.forEach(function(file) {
+      file.disabled = true;
+    });
+    
+    // Enable the selected CSS file
+    var selectedTheme = document.getElementById(themeId);
+    if (selectedTheme) {
+      selectedTheme.disabled = false;
+
+      
+      localStorage.setItem('activeTheme', themeId);
+      document.body.style.visibility = 'visible';
+    }
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var activeTheme = localStorage.getItem("activeTheme");
+    if (activeTheme) {
+      activateTheme(activeTheme);
+    }else {
+      document.body.style.visibility = 'hidden';
+    }
+
+});
