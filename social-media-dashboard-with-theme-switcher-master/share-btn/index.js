e0ecada15e0e79abc8494188e560e65217609726
feranmi <feranmygit@gmail.com>
2024-04-11 08:15:38 -0700

    made some changes to my stylings

---
+++ social-media-dashboard-with-theme-switcher-master/share-btn/index.js
@@ -0,0 +1,5 @@
+document.getElementById('share-btn').addEventListener('click', function() {
+    // Toggle the visibility of the share links
+    document.getElementById('share-links').classList.toggle('show');
+  });
+  
