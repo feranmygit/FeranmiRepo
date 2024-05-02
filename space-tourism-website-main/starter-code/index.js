d0a17d3d2a15f7d0834c8f1b189147deba6e87ee
feranmi <feranmygit@gmail.com>
2024-05-02 09:21:24 -0700

    I styled the menu container

--- space-tourism-website-main/starter-code/index.js
+++ space-tourism-website-main/starter-code/index.js
@@ -1,5 +1,3 @@
-var menu = document.getElementById('menu-box');
 var hamburger = document.getElementById('hamburger').addEventListener('click', function(){
-    document.getElementById('menu-box').classList.toggle('show');
-    // menu.style.right = menu.style.right === '0px' ? '-300px' : '0px';
+    document.getElementById('remove').classList.toggle('show');
 });
