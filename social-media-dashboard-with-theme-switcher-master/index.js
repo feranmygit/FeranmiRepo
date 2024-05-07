72136478c1cf7bf39f977b9915e809e71a9e79df
feranmi <feranmygit@gmail.com>
2024-05-07 08:12:53 -0700

    I made changes to the style of social media theme switcher, which i added grip to it for responsive design

--- social-media-dashboard-with-theme-switcher-master/index.js
+++ social-media-dashboard-with-theme-switcher-master/index.js
@@ -26,41 +26,41 @@ function updateContainers(){
     });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    updateContainers();
-})
+// document.addEventListener('DOMContentLoaded', function() {
+//     updateContainers();
+// })
 
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+// function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
-function handleScroll() {
-    const hiddenContainer = document.querySelectorAll('.hiddenContainer');
-    const shakingContainer =document.querySelectorAll('#shakingContainer');
-    shakingContainer.forEach(shakingContainer => {
-        if (isInViewport(shakingContainer)) {
-            shakingContainer.classList.add('shaking');
-        } else {
-            shakingContainer.classList.remove('shaking');
-        }
-    })
+// function handleScroll() {
+//     const hiddenContainer = document.querySelectorAll('.hiddenContainer');
+//     const shakingContainer =document.querySelectorAll('#shakingContainer');
+//     shakingContainer.forEach(shakingContainer => {
+//         if (isInViewport(shakingContainer)) {
+//             shakingContainer.classList.add('shaking');
+//         } else {
+//             shakingContainer.classList.remove('shaking');
+//         }
+//     })
 
-    hiddenContainer.forEach(hiddenContainer => {
-        if (isInViewport(hiddenContainer)) {
-            hiddenContainer.classList.add('visible');
-            hiddenContainer.classList.remove('hidden');
-        } else {
-            hiddenContainer.classList.remove('visible');
-            hiddenContainer.classList.add('hidden');
-        }
-    });
-}
+//     hiddenContainer.forEach(hiddenContainer => {
+//         if (isInViewport(hiddenContainer)) {
+//             hiddenContainer.classList.add('visible');
+//             hiddenContainer.classList.remove('hidden');
+//         } else {
+//             hiddenContainer.classList.remove('visible');
+//             hiddenContainer.classList.add('hidden');
+//         }
+//     });
+// }
 
-window.addEventListener('scroll', handleScroll);
+// window.addEventListener('scroll', handleScroll);
