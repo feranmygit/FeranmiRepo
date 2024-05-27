36d5005f2ed9dded97d9a5d892fa5d5f87a1b347
feranmi <feranmygit@gmail.com>
2024-05-27 15:19:30 -0700

    I'm working on javascript

---
+++ my-path/new-class-doc/index.js
@@ -0,0 +1,32 @@
+// const name = "Chris";
+// const greeting = `Hello, ${name}`;
+// console.log(greeting);// "Hello, Chris"
+// window.alert(greeting);
+
+// let num = 15;
+// num += 5;
+// window.alert(num);
+
+
+// let x = prompt('what is your name');
+// const my_name = document.getElementById('num').innerHTML = x;
+
+// let first_num = 20;
+// let second_num = '20';
+// alert(first_num==second_num);
+
+// let first_num1 = 15;
+// let second_num2 = 15;
+// alert(first_num1===second_num2);
+
+// let ant = true;
+// let boy = 7;
+// let cat = 5;
+// let dog = boy >7 ? ant : cat == 5;
+
+// alert(dog);
+
+let a = 5;
+let b = a >4 ? 'sign-in successful' : 'sign-in failed';
+const my_name = document.getElementById('num').innerHTML = b;
+alert(b);
